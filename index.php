d8cb6ddc73d8dffbb026078f870d71734bce74de
aaradhy25tiwari <aaradhy.tiwari2584@gmail.com>
2024-08-27 09:12:09 +0530

    Added index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MeDDeck</title>
+</head>
+<body>    
+    <!--Header section starts here-->
+    <section class="header">
+        <img src="images/logo.jpg" alt="">
+        <a href="index.php" class="logo">MeDDeck</a>
+        <nav class="navbar">
+            <a href="home.php">home</a>
+            <a href="about.php">about</a>
+            <a href="package.php">package</a>
+            <a href="book.php">book</a>
+        </nav>
+        <div id="menu-btn" class="fas fa-bars"></div>
+    </section>
+    <!--Header section ends here-->
+</body>
+</html>
