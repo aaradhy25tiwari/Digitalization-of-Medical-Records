bc151b24977500e5a5cdeebebb86f57fdaf6d8a0
aaradhy25tiwari <aaradhy.tiwari2584@gmail.com>
2024-09-01 19:54:56 +0530

    Updated welcome.php

---
+++ welcome.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MeDDeck</title>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel="stylesheet" href="style.css" />
+    <link rel="shortcut icon" href="Images/icon.ico" type="image/x-icon" />
+</head>
+  <body>
+        <h1 style="display:flex; text-align:center; justify-content:center; align-items:center;"> Congratulation !!! Login Successfull!</h1>
+  </body>
+</html>
