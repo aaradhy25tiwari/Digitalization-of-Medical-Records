06bb0829ef85d3d6736e662000fb7d66fefa88df
aaradhy25tiwari <aaradhy.tiwari2584@gmail.com>
2024-09-17 22:51:33 +0530

    Updated Doctor files

--- Doctor/php/register.php
+++ Doctor/php/register.php
@@ -10,7 +10,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if (isset($_POST["email"])) {
         $check_email = $_POST["email"];
-        $sql_check = "SELECT * FROM signup WHERE username = '$check_email'";
+        $sql_check = "SELECT * FROM doctor_signup WHERE username = '$check_email'";
         $result = mysqli_query($conn, $sql_check);
 
         if (mysqli_num_rows($result) > 0) {
@@ -27,6 +27,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $password = password_hash($_POST["pass"], PASSWORD_DEFAULT); // Hash password for security
     $cpassword = password_hash($_POST["cpass"], PASSWORD_DEFAULT); // Hash confirm password
     $blood = $_POST["blood"];
+    $id = $_POST["id"];
   
     /*// Check if password and confirm password match
     if ($password != $cpassword) {
@@ -35,14 +36,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }*/
   
     // Prepare SQL statement with parameterized query for security
-    $sql = "INSERT INTO signup (phone, username, password, bloodType) VALUES (?, ?, ?, ?)";
+    $sql = "INSERT INTO doctor_signup (phone, username, password, bloodType, UID) VALUES (?, ?, ?, ?, ?)";
     $stmt = $conn->prepare($sql);
-    $stmt->bind_param("ssss", $phone, $email, $password, $blood);
+    $stmt->bind_param("sssss", $phone, $email, $password, $blood, $id);
   
     // Execute the query
     if ($stmt->execute()) {
       echo "New user created successfully!";
-      header("Location: ./login.html");
+      header("Location: ../login.html");
     } else {
       echo "Error: " . $sql . "<br>" . $conn->error;
     }
